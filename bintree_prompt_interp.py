9f1e1302082ba72745a0cfccc2adeabbdf63363a
Felix Niemeyer <niemeyer.felix@gmail.com>
2024-11-21 10:35:04 +0100

    make bintreeinterp work

--- bintree_prompt_interp.py
+++ bintree_prompt_interp.py
@@ -24,7 +24,7 @@ class Interpolator:
 
     def __init__(
         self,
-        output_prefix,
+        subfolder,
         comfy_output_folder,
         prompt_from, 
         prompt_to,
@@ -33,7 +33,8 @@ class Interpolator:
 
         # load next_fram_gen.json workflow and find relevant nodes
         self.conditioning_average = None
-        self.comfy_output_folder = comfy_output_folder
+
+        save_image = None
 
         with open(workflow_file) as f:
             workflow = json.load(f)
@@ -42,33 +43,35 @@ class Interpolator:
                 node = workflow[node_id]
                 class_type = node["class_type"]
                 if class_type == "SaveImage":
-                    node["inputs"]["filename_prefix"] = output_prefix
+                    save_image = node
                 if class_type == "ConditioningAverage": 
                     self.conditioning_average = node
-                    from_prompt_id = node["inputs"]["conditioning_from"]
-                    to_prompt_id = node["inputs"]["conditioning_to"]
+                    from_prompt_id = node["inputs"]["conditioning_from"][0]
+                    to_prompt_id = node["inputs"]["conditioning_to"][0]
                     workflow[from_prompt_id]["inputs"]["text"] = prompt_from
                     workflow[to_prompt_id]["inputs"]["text"] = prompt_to
             self.workflow = workflow
 
-        if save_image == None:
-            raise Exception("Could not find SaveImage in workflow", workflow_file)
         if self.conditioning_average == None:
             raise Exception("Could not find ConditioningAverage in workflow", workflow_file)
+        if save_image == None:
+            raise Exception("Could not find SaveImage in workflow", workflow_file)
 
         # get absolute path of output folder
         output_folder = os.path.abspath(comfy_output_folder)
+        output_folder = os.path.join(output_folder, subfolder)
 
         # get absolute output path
         i = 0
         ok = False
         while not ok:
-            self.output_folder = os.path.join(output_folder, output_prefix)
-            self.output_folder = os.path.dirname(self.output_folder) + str(i)
+            self.output_folder = os.path.join(output_folder, str(i))
             if os.path.exists(self.output_folder):
                 i += 1
             else:
                 ok = True
+
+        save_image["inputs"]["filename_prefix"] = f"{subfolder}/{i}/f"
 
     runned = False
     def run(self, frame_count):
@@ -80,7 +83,7 @@ class Interpolator:
 
         self.initialize_tree()
 
-        for i in range(frame_count - 3):
+        for frame_id in range(frame_count - 3):
             # find largest gap
             largest_gap_size = 0
             largest_gap_id = 0
@@ -89,18 +92,23 @@ class Interpolator:
                     largest_gap_size = self.gaps[i]
                     largest_gap_id = i
 
-            p = 0.5 * (self.frames[largest_gap_id].p + self.frames[largest_gap_id + 1].p)
+            left_frame = self.frames[largest_gap_id]
+            right_frame = self.frames[largest_gap_id + 1]
 
-            frame = get_frame_at(p)
+            p = 0.5 * (left_frame.p + right_frame.p)
+
+            frame = self.get_frame_at(p)
 
             # insert frame at the right position
             self.frames.insert(largest_gap_id + 1, frame)
 
             # update gaps
-            gap_left = img_distance(self.frames[largest_gap_id], frame) 
-            gap_right = img_distance(frame, self.frames[largest_gap_id + 1])
+            gap_left = self.img_distance(left_frame, frame) 
+            gap_right = self.img_distance(frame, right_frame)
             self.gaps[largest_gap_id] = gap_left
             self.gaps.insert(largest_gap_id + 1, gap_right)
+
+            print('updated gaps', self.gaps)
 
         # rename all files to be in order
         for i in range(len(self.frames)):
@@ -112,32 +120,34 @@ class Interpolator:
     def initialize_tree(self):
         # run workflow for conditioning_to_strength 0, 0.5, 1 
         self.frames = [
-            get_frame_at(0.0, workflow), 
-            get_frame_at(0.5, workflow), 
-            get_frame_at(1.0, workflow)
+            self.get_frame_at(0.0), 
+            self.get_frame_at(0.5), 
+            self.get_frame_at(1.0)
         ]
         self.gaps = [
-            img_distance(tree[0], tree[1]), 
-            img_distance(tree[1], tree[2])
+            self.img_distance(self.frames[0], self.frames[1]),
+            self.img_distance(self.frames[1], self.frames[2])
         ]
     
-    out_id = 0
-    def get_frame_at(p):
+    out_id = 1
+    def get_frame_at(self, p):
         print(f"generating frame at {p}")
 
         self.conditioning_average["inputs"]["conditioning_to_strength"] = p
 
-        workflow_string = json.dumps(workflow)
+        workflow_string = json.dumps(self.workflow)
 
         # save to file
         with open('./last_enqueued_workflow.json', "w") as f:
             f.write(workflow_string)
 
-        expected_filepath = os.path.join(self.comfy_output_folder, f"{output_prefix}_{out_id:05}_.png")
+        expected_filepath = os.path.join(self.output_folder, f"f_{self.out_id:05}_.png")
+        self.out_id += 1
 
         queue_prompt(workflow_string)
 
         # wait for file to appear
+        print('waiting for', expected_filepath)
         while not os.path.exists(expected_filepath):
             time.sleep(0.5)
 
@@ -146,13 +156,15 @@ class Interpolator:
             p
         )
 
-    def img_distance(frame1, frame2):
+    def img_distance(self, frame1, frame2):
         # calculate Mean Squared Error 
         img1 = imread(frame1.path)
         img2 = imread(frame2.path)
 
         # Calculate MSE across all channels
-        return = np.mean((img1 - img2) ** 2)
+        distance = np.mean((img1 - img2) ** 2)
+
+        return distance.item()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='bintree prompt travel generates frames from one prompt to another always filling the gaps with the largest distance')
@@ -161,7 +173,7 @@ if __name__ == "__main__":
     parser.add_argument('prompt_to', type=str, help='Prompt to')
 
     parser.add_argument('comfy_output_folder', type=str, help='Comfy output folder')
-    parser.add_argument('--output_prefix', '-p', type=str, help='like in ImageSave node', default="bininterp/v/f")
+    parser.add_argument('--out_path', '-p', type=str, help='folder within comfy output folder', default="bininterp")
     parser.add_argument('--workflow', '-w', type=str, default="workflows/bintree_prompt_interp/v0.json", help='Workflow file to use')
 
     parser.add_argument('--frame_count', '-n', type=int, default=10, help='Number of frames to generate')
@@ -169,7 +181,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     interp = Interpolator(
-        args.output_prefix,
+        args.out_path,
         args.comfy_output_folder,
         args.prompt_from,
         args.prompt_to,
@@ -177,3 +189,4 @@ if __name__ == "__main__":
     )
 
     interp.run(args.frame_count)
+
